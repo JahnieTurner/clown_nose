4943e97a811112250441e69edc7af747e372ec80
JahnieTurner <80420943+JahnieTurner@users.noreply.github.com>
2022-01-02 19:43:22 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function preload() {
+
+}
+
+function setup() {
+    canvas = createCanvas(400, 400)
+    canvas.center()
+    video = createCapture(VIDEO)
+
+}
+
+function draw() {
+
+}
+
+function takesnapshot() {
+    save('clow_nose.png')
+}
